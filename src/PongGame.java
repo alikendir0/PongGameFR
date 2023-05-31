69f293533f1c10acddffcebe91d364b5b8d421ec
alikendir0 <alikendir2002@gmail.com>
2023-05-31 13:14:26 +0300

    test

--- src/PongGame.java
+++ src/PongGame.java
@@ -1,5 +1,7 @@
 public class PongGame {
     public static void main(String[] args) {
         new GameFrame();
+        System.out.println("have fun");
+        System.out.println("huh");
     }
 }
